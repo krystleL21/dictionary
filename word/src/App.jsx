7f1b43332c8554fd3841e682c05b162216ed2a77
krystleL21 <54385637+krystleL21@users.noreply.github.com>
2025-08-20 07:47:59 -0400

    Added dictionary compo. and fixed 404 error

--- word/src/App.jsx
+++ word/src/App.jsx
@@ -1,16 +1,28 @@
-import kluxlogo from './kluxlogo.png'
-import './App.css'
+import React from 'react';
+import kluxlogo from './kluxlogo.png';
+import './App.css';
+import Dictionary from './Dictionary';
 
 
 function App() {
   
   return (
     <div className="App">
+      <div className="container">
       <header className="App-header">
         <img src={kluxlogo} className="App-logo img-fluid" alt="logo" />
       </header>
-      <h1>Hello</h1>
+      <main>
+        <Dictionary />
+      </main>
+      <footer className="App-footer">
+        
+        <small>Coded by K-Lux</small>
+
+      </footer>
+      </div>
     </div>
+
   )
 }
 export default App
